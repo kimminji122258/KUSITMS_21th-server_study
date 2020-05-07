0b30c88074fd054892cb2d65f2b3bcf5f60ca2e4
kimminji122258 <kimminji122258@gmail.com>
2020-05-07 19:48:06 +0900

    0507 study commit

--- 01_hello_world/app.py
+++ 01_hello_world/app.py
@@ -9,5 +9,10 @@ def index():
     username = request.args.get("username")
     return render_template("index.html", username=username)
 
+
+@app.route("/<username>")
+def index_by_username(username):
+    return render_template("index.html", username=username)
+
 if __name__ == "__main__":
     app.run()
