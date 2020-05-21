99182754a70d29fa96dc0d506e753208f7a04051
kimminji122258 <kimminji122258@gmail.com>
2020-05-21 19:43:08 +0900

    0521 commit

--- 03_rest_basic/apps/user/views.py
+++ 03_rest_basic/apps/user/views.py
@@ -1,3 +1,8 @@
 #서버의 요청을 처리하는 것들이 여기로 오게 된다.
 from flask_restful import Resource
 
+class UserAPI(Resource):
+    def get(self, *args, **kwargs):
+        return {
+            "msg": "hello world!"
+        }
