5a718749cd188b0f8f827e8524ce7cf0af59ff34
kimminji122258 <kimminji122258@gmail.com>
2020-05-21 20:44:34 +0900

    0521

--- 03_rest_basic/app.py
+++ 03_rest_basic/app.py
@@ -3,10 +3,11 @@ from utils.database import db, migrate
 from apps.endpoints import api
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
 
 """Initializing plugins"""
 db.init_app(app)
-migrate.init_app(app)
+migrate.init_app(app, db)
 api.init_app(app)
 
 if __name__ == '__main__':
